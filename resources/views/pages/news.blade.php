160e89731a7287d29c7bcc09222c23c358ffea5d
Gianath <94115684+Gianath@users.noreply.github.com>
2023-01-16 16:20:24 +0700

    Fix: profile dropdown is covered by latest news

--- resources/views/pages/news.blade.php
+++ resources/views/pages/news.blade.php
@@ -5,12 +5,14 @@
 @section('content')
     @include('partials.navbar')
 
-    <main class="w-full flex px-hor">
+    <main class="w-full flex px-hor flex-wrap">
+        <div class="w-full">
+            <h1 class="text-headline-m font-bold desktop-s:text-headline-l w-4/5">{{ $news->title }}</h1>
+        </div>
         <div class="w-4/5">
-            <h1 class="text-headline-m font-extrabold">{{ $news->title }}</h1>
             <p>{{ date_format($news->created_at, 'd M Y') }}. by {{ $news->user->name }}</p>
             <div class="flex items-end">
-                <p class="leading-5 px-1">{{$news->likes->count()}}</p>
+                <p class="leading-5 px-1">{{ $news->likes->count() }}</p>
                 <img src="{{ asset('images/like.svg') }}" alt="">
             </div>
             <img src="{{ asset('storage/images/lifestyle-placeholder.jpg') }}" alt="">
@@ -24,14 +26,14 @@
             <p>{{ $news->content }}</p>
         </div>
         <div class="w-1/5 relative">
-            <div class="fixed pr-hor">
+            <div class="sticky top-0">
                 <h1 class="text-headline-m text-center font-extrabold">Latest News</h1>
                 <div class="flex flex-col gap-4">
                     @foreach ($latest_news as $news)
                         <div class="px-2 py-1 border-2 border-black">
                             <h1 class="font-extrabold">{{ $news->title }}</h1>
                             <p class="py-3 font-semibold">{{ date_format($news->created_at, 'd M Y') }}</p>
-                            <a href={{ "/news/" . $news->slug }} class="text-primary-600 underline">Read article</a>
+                            <a href={{ '/news/' . $news->slug }} class="text-primary-600 underline">Read article</a>
                         </div>
                     @endforeach
                 </div>
