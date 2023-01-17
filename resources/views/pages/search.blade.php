885926e17d754bd23265e8ebc4916c33e83af496
Adrian-py <62276355+Adrian-py@users.noreply.github.com>
2023-01-17 19:42:23 +0700

    Fix: change styling for search

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -8,7 +8,7 @@
         <h1 class="text-headline-s font-bold desktop-s:text-headline-l">Find Any News</h1>
         <form class="my-[1rem] flex justify-between gap-[2vw]">
             <input type="search" name="search" class="px-[1rem] border-2 border-neutrals-400 grow" id="search" placeholder="Search">
-            <button type="submit" class="w-fit px-[1rem] py-[0.5rem] bg-primary-600 text-white-100">Search</button>
+            <button type="submit" class="w-fit px-[1rem] py-[0.5rem] bg-primary-600 text-white-100 rounded-lg">Search</button>
         </form>
         @include("partials.news-list")
     </section>
